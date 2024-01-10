ff5064bc59065000d5abdf4bf3cc5f83749e4858
AMEZIAN Fati AZAHRA <amezianfati440@gmail.com>
2024-01-10 17:27:58 +0100

    Creation de la classe Produit.java

---
+++ src/main/java/ma/ExamDevops/App.java
@@ -0,0 +1,13 @@
+package ma.ExamDevops;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/ma/ExamDevops/Product.java
@@ -0,0 +1,5 @@
+package ma.ExamDevops;
+
+public class Product {
+
+}
